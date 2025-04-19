0201e966342203e4da3e20aea222bba2050c98e3
JonathanClara <se3bclara@gmail.com>
2025-04-19 03:10:51 +0200

    Terminado el generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
+
+const iconos = ["♥", "♦", "♣", "♠"];
+const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateRandomCard() {
+    let icono = getRandomElement(iconos);
+    const numero = getRandomElement(numeros);
+
+    if (icono === "♠" || icono === "♣") {
+        icono = getRandomElement(["♠", "♣"]);
+    } else if (icono === "♥" || icono === "♦") {
+        icono = getRandomElement(["♥", "♦"]);
+    }
+    
+   
+    const carta = document.getElementById("carta");
+    const iconoSuperior = document.getElementById("icono-superior");
+    const iconoInferior = document.getElementById("icono-inferior");
+    const numeroElemento = document.getElementById("numero");
+
+    iconoSuperior.textContent = icono;
+    iconoInferior.textContent = icono;
+    numeroElemento.textContent = numero;
+
+    const esRojo = icono === "♥" || icono === "♦";
+    carta.style.color = esRojo ? "red" : "black";
+   
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+generateRandomCard();
